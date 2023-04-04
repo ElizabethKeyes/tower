12903af1f7c7092b6ad67821a65dfe88d0984e77
Elizabeth Keyes <liz4momentum@gmail.com>
2023-04-04 16:40:29 -0600

    env info updated

--- tower.client/src/env.js
+++ tower.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-rgok7xvssd776rhv.us.auth0.com'
+export const clientId = 'uorUrLI0L7SetJtJj4JaQcUD2LWN1oIp'
+export const audience = 'http://sandbox.io'
