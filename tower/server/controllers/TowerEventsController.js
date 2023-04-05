1e2ec7efba5ecaf750e29d17ff3c1d68f50b9094
Elizabeth Keyes <liz4momentum@gmail.com>
2023-04-04 19:24:12 -0600

    All event get & put requests functioning properly

--- tower/server/controllers/TowerEventsController.js
+++ tower/server/controllers/TowerEventsController.js
@@ -6,10 +6,32 @@ export class TowerEventsController extends BaseController {
   constructor() {
     super('/api/events')
     this.router
+      .get('', this.getAllTowerEvents)
+      .get('/:towerEventId', this.getTowerEventById)
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('', this.createTowerEvent)
+      .put('/:towerEventId', this.editTowerEvent)
+      .delete('/:towerEventId', this.cancelEvent)
   }
 
+  async getAllTowerEvents(req, res, next) {
+    try {
+      const towerEvents = await towerEventsService.getAllTowerEvents()
+      res.send(towerEvents)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getTowerEventById(req, res, next) {
+    try {
+      const towerEventId = req.params.towerEventId
+      const towerEvent = await towerEventsService.getTowerEventById(towerEventId)
+      res.send(towerEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
 
   async createTowerEvent(req, res, next) {
     try {
@@ -17,6 +39,27 @@ export class TowerEventsController extends BaseController {
       eventData.creatorId = req.userInfo.id
       const towerEvent = await towerEventsService.createTowerEvent(eventData)
       res.send(towerEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async editTowerEvent(req, res, next) {
+    try {
+      const towerEventId = req.params.towerEventId
+      const towerEventData = req.body
+      const towerEvent = await towerEventsService.editTowerEvent(towerEventId, towerEventData)
+      res.send(towerEvent)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async cancelEvent(req, res, next) {
+    try {
+      const towerEventId = req.params.towerEventId
+      // TODO come back and finish soft delete function
+      res.send()
     } catch (error) {
       next(error)
     }
--- tower/server/services/TowerEventsService.js
+++ tower/server/services/TowerEventsService.js
@@ -1,10 +1,35 @@
 import { dbContext } from "../db/DbContext.js"
 
 class TowerEventsService {
-  async createTowerEvent(eventData) {
-    const towerEvent = await dbContext.TowerEvent.create(eventData)
+  async getAllTowerEvents() {
+    const towerEvents = await dbContext.TowerEvents.find()
+    return towerEvents
+  }
+
+  async getTowerEventById(towerEventId) {
+    const towerEvent = await dbContext.TowerEvents.findById(towerEventId)
     return towerEvent
   }
+
+  async createTowerEvent(eventData) {
+    const towerEvent = await dbContext.TowerEvents.create(eventData)
+    return towerEvent
+  }
+
+  async editTowerEvent(towerEventId, towerEventData) {
+    const towerEvent = await dbContext.TowerEvents.findById(towerEventId)
+    towerEvent.name = towerEventData.name ? towerEventData.name : towerEvent.name
+    towerEvent.description = towerEventData.description ? towerEventData.description : towerEvent.description
+    towerEvent.coverImg = towerEventData.coverImg ? towerEventData.coverImg : towerEvent.coverImg
+    towerEvent.location = towerEventData.location ? towerEventData.location : towerEvent.location
+    towerEvent.capacity = towerEventData.capacity ? towerEventData.capacity : towerEvent.capacity
+    towerEvent.startDate = towerEventData.startDate ? towerEventData.startDate : towerEvent.startDate
+    towerEvent.isCanceled = towerEvent.isCanceled
+    towerEvent.type = towerEventData.type ? towerEventData.type : towerEvent.type
+    await towerEvent.save()
+    return towerEvent
+  }
+
 
 }
 
